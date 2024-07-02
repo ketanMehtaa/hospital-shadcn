dc23b4917ff0ffff388a6cfd22d7f782bca3f257
ketan mehta <meetketanmehta@gmail.com>
2024-07-02 17:08:44 +0530

    google map added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,23 @@ export default function Home() {
         <Header />
         <Startcarousel />
         <div>
-          <h2 className="text-2xl font-semibold text-center my-4 md:text-3xl lg:text-4xl">
+          <h2 className="text-xl font-semibold text-center my-4 md:text-3xl lg:text-4xl">
             Book Your Appointment for Online or Offline Consultation.
           </h2>
           <CalEmbed />
+        </div>
+        <div className="flex justify-center flex-col p-5">
+          <h2 className="text-xl font-semibold text-center my-4 md:text-3xl lg:text-4xl">Locate Us.</h2>
+          <div className="flex justify-center md:h-dvh h-96">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d435.3216076332275!2d79.4932994!3d29.2066999!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39a09b1ede17394d%3A0x823b92fb077c3fb6!2sSHARDA%20ENT%20HOSPITAL%20AND%20DIAGNOSTIC%20CENTER!5e0!3m2!1sen!2sin!4v1719919363709!5m2!1sen!2sin"
+              style={{ border: '0' }}
+              loading="lazy"
+              height="100%"
+              width="100%"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
         </div>
       </main>
     </>
